e61ebb6f5ef050fd25b8224ee42e9c9f1392894e
vera <vero971410@gmail.com>
2022-11-15 11:30:36 +0300

    PlusOne #66

---
+++ src/easy/PlusOne_66.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class PlusOne_66 {
+    // https://leetcode.com/problems/plus-one/solutions/
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+}
