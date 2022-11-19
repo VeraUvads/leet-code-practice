612cc9a42b6eaeba0790a140efaeb36a3996482b
vera <vero971410@gmail.com>
2022-11-19 20:19:54 +0300

    RemoveDuplicatesFromSortedList #83

---
+++ src/easy/RemoveDuplicatesFromSortedList_83.java
@@ -0,0 +1,34 @@
+package easy;
+
+public class RemoveDuplicatesFromSortedList_83 {
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
