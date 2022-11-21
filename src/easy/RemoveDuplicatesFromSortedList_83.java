210cba11355ab665beb83694a9ec4042338e3fc2
vera <vero971410@gmail.com>
2022-11-21 14:52:23 +0300

    SameTree #100

--- src/easy/RemoveDuplicatesFromSortedList_83.java
+++ src/easy/RemoveDuplicatesFromSortedList_83.java
@@ -1,23 +1,13 @@
 package easy;
 
+import java.util.Stack;
+
 public class RemoveDuplicatesFromSortedList_83 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
 
     public static class ListNode {
         int val;
         ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
     }
 
     public ListNode deleteDuplicates(ListNode head) {
@@ -32,3 +22,4 @@ public class RemoveDuplicatesFromSortedList_83 {
         return head;
     }
 }
+
---
+++ src/easy/SameTree_100.java
@@ -0,0 +1,18 @@
+package easy;
+
+public class SameTree_100 {
+    // https://leetcode.com/problems/same-tree/description/
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if ((p == null) != (q == null)) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
