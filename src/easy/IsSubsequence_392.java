67d44ca385988b276abda33318efd4ec9e4829f5
vera <vero971410@gmail.com>
2022-11-27 14:54:06 +0300

    IsSubsequence #392

---
+++ src/easy/IsSubsequence_392.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class IsSubsequence_392 {
+
+    public boolean isSubsequence(String s, String t) {
+        int sIndex = 0;
+        int tIndex = 0;
+        while (sIndex < s.length() && tIndex < t.length()) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                sIndex++;
+            }
+            tIndex++;
+
+        }
+        return sIndex == s.length();
+    }
+}
