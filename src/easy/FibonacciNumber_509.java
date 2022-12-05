737378e33d0329c62181a2a26fb19ed91e4befb3
vera <vero971410@gmail.com>
2022-12-05 12:34:50 +0300

    FibonacciNumber #509

---
+++ src/easy/FibonacciNumber_509.java
@@ -0,0 +1,22 @@
+package easy;
+
+public class FibonacciNumber_509 {
+    // https://leetcode.com/problems/fibonacci-number/description/?envType=study-plan&id=level-1
+
+
+    public int fib(int n) {
+        if (n == 1) return 1;
+        if (n == 0) return 0;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public int fibDP(int n) {
+        if (n == 1 || n == 0) return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
