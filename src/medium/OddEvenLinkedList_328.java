c7164d5be1965f469f00c8bf13d40e2a273d14a1
vera <vero971410@gmail.com>
2022-12-07 11:08:40 +0300

    OddEvenLinkedList #328

---
+++ src/medium/OddEvenLinkedList_328.java
@@ -0,0 +1,25 @@
+package medium;
+
+public class OddEvenLinkedList_328 {
+    public class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        int index = 1;
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = head.next;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+}
