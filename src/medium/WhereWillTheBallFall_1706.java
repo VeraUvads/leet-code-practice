c39dadfeb82f44c706588ee1408c1da86d872b81
vera <vero971410@gmail.com>
2022-12-12 11:32:32 +0300

    WhereWillTheBallFall #1706

---
+++ src/medium/WhereWillTheBallFall_1706.java
@@ -0,0 +1,29 @@
+package medium;
+
+public class WhereWillTheBallFall_1706 {
+    // https://leetcode.com/problems/where-will-the-ball-fall/?envType=study-plan&id=level-2
+
+    public static int[] findBall(int[][] grid) {
+        if (grid.length == 0) return new int[0];
+        int[] result = new int[grid[0].length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = i;
+        }
+        for (int column = 0; column < grid[0].length; column++) {
+            int curColumn = column;
+            for (int[] row : grid) {
+                int next = curColumn + row[curColumn];
+                if (next < 0 || next > row.length - 1 || row[curColumn] != row[next]) {
+                    curColumn = -1;
+                } else if (curColumn != -1) {
+                    curColumn = next;
+                }
+            }
+            result[column] = curColumn;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+    }
+}
