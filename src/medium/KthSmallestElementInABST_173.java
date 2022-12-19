48976c32b5148939d3662bb5b005ca50e2c973a9
vera <vero971410@gmail.com>
2022-12-19 23:07:56 +0300

    KthSmallestElementInABST #173

---
+++ src/medium/KthSmallestElementInABST_173.java
@@ -0,0 +1,36 @@
+package medium;
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class KthSmallestElementInABST_173 {
+
+    private List<Integer> sorted = new ArrayList<>();
+    public int kthSmallest(TreeNode root, int k) {
+        inorderTraversal(root);
+        return sorted.get(k - 1);
+    }
+
+    private void inorderTraversal(TreeNode node) {
+        if (node == null) return;
+        inorderTraversal(node.left);
+        sorted.add(node.val);
+        inorderTraversal(node.right);
+    }
+
+    public int kthSmallestIterative(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (true) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (k == 1) return root.val;
+            root = root.right;
+        }
+    }
+}
