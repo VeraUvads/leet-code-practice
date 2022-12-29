089fb53d44878fa0fee5ac585506f05a508a4d67
vera <vero971410@gmail.com>
2022-12-29 15:51:52 +0300

    AsteroidCollision #735

---
+++ src/medium/AsteroidCollision_735.java
@@ -0,0 +1,29 @@
+package medium;
+
+import java.util.Stack;
+
+public class AsteroidCollision_735 {
+
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            boolean isExplode = false;
+            while (!stack.isEmpty() && asteroid < 0) {
+                if (stack.peek() < 0)  break;
+
+                int prev = stack.pop();
+                isExplode = -prev == asteroid;
+                if (isExplode) break;
+                if (asteroid + prev > 0) {
+                    asteroid = prev;
+                }
+            }
+            if (!isExplode) stack.add(asteroid);
+        }
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
