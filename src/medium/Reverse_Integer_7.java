0c1466a1034e38babe5e19bfbc2d6b7b6e1f0f92
vera <vero971410@gmail.com>
2023-01-09 16:32:26 +0300

    Reverse Integer #7

---
+++ src/medium/Reverse_Integer_7.java
@@ -0,0 +1,21 @@
+package medium;
+
+public class Reverse_Integer_7 {
+    // https://leetcode.com/problems/reverse-integer/description/
+
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (
+                    result > Integer.MAX_VALUE / 10 ||
+                            (result == Integer.MAX_VALUE / 10 && pop > 7) ||
+                            result < Integer.MIN_VALUE / 10 ||
+                            (result < Integer.MIN_VALUE / 10 && pop < -8)
+            ) return 0;
+            result = result * 10 + pop;
+        }
+        return result;
+    }
+}
