073c2460c06802dbfa43b680c5307625ae588f2a
vera <vero971410@gmail.com>
2023-01-15 09:53:00 +0300

    366. Find Leaves of Binary Tree

---
+++ src/medium/FindLeavesOfBinaryTree_366.java
@@ -0,0 +1,30 @@
+package medium;
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindLeavesOfBinaryTree_366 {
+    List<List<Integer>> leafs = new ArrayList<>();
+
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        dfs(root);
+        return leafs;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return -1;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        int index = Math.max(left, right) + 1;
+        if (leafs.size() < index + 1) {
+            leafs.add(new ArrayList<>());
+        }
+        leafs.get(index).add(root.val);
+
+        root.left = null;
+        root.right = null;
+        return index;
+    }
+}
