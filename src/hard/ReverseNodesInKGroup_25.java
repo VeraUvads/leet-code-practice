3f35e9ec0212a7c53ecb43387eb7aab6c2d21e05
vera <vero971410@gmail.com>
2023-02-09 17:44:29 +0300

    ReverseNodesInKGroup #25

--- src/hard/ReverseNodesInKGroup_25.java
+++ src/hard/ReverseNodesInKGroup_25.java
@@ -5,7 +5,7 @@ import utils.ListNode;
 public class ReverseNodesInKGroup_25 {
     // https://leetcode.com/problems/reverse-nodes-in-k-group/description/
 
-    public ListNode reverseKGroup(ListNode head, int k) {
+    public ListNode reverseKGGroupRecursive(ListNode head, int k) {
         if (head == null) return null;
         ListNode lastAtTime = head;
         int count = 0;
@@ -36,4 +36,32 @@ public class ReverseNodesInKGroup_25 {
         }
         return previous;
     }
+
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+        ListNode dummy = new ListNode();
+        ListNode dummyHolder = dummy;
+        ListNode lastAtTime = head;
+        while (lastAtTime != null) {
+            int count = 0;
+            ListNode temp = lastAtTime;
+            while (lastAtTime != null && count < k) {
+                lastAtTime = lastAtTime.next;
+                count++;
+            }
+            if (count == k) {
+                dummy.next = reverse(head, k);
+                head = lastAtTime;
+                while (dummy.next != null) {
+                    dummy = dummy.next;
+                }
+            } else {
+                dummy.next = temp;
+            }
+        }
+        return dummyHolder.next;
+    }
+
+
 }
