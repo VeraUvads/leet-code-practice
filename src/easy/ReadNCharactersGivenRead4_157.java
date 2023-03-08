7456086f5ee7fddef4fe2ca69edc4a7ac32ffccc
vera <vero971410@gmail.com>
2023-03-08 11:54:33 +0100

    ReadNCharactersGivenRead4 #157

---
+++ src/easy/ReadNCharactersGivenRead4_157.java
@@ -0,0 +1,23 @@
+package easy;
+
+import utils.Reader4;
+
+public class ReadNCharactersGivenRead4_157 extends Reader4 {
+
+    public int read(char[] buf, int n) {
+        int pointer = 0;
+        char[] internalBuf = new char[4];
+        while (pointer < n) {
+            int readed = read4(internalBuf);
+            for (int i = 0; i < readed; i++) {
+                buf[pointer++] = internalBuf[i];
+                if (pointer == n) break;
+            }
+            if (readed < 4) break;
+        }
+
+        return pointer;
+    }
+
+
+}
---
+++ src/utils/Reader4.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Reader4 {
+
+    protected int read4(char[] internalBuf) {
+        return 0;
+    }
+
+}
