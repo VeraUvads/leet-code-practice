e3c8828d7affdfb37587bb9258e324aad48b673a
vera <vero971410@gmail.com>
2023-03-20 12:39:57 +0100

    Single_Row_Keyboard #1165

---
+++ src/easy/Single_Row_Keyboard_1165.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class Single_Row_Keyboard_1165 {
+    // https://leetcode.com/problems/single-row-keyboard/description/
+    public int calculateTime(String keyboard, String word) {
+        if (word.length() == 0) return 0;
+        int[] positions = new int[26];
+        for (int i = 0; i < keyboard.length(); i++) {
+            positions[keyboard.charAt(i) - 'a'] = i;
+        }
+        int counter = 0;
+        int prev = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            int currPosition = positions[letter - 'a'];
+            counter += Math.abs(prev - currPosition);
+            prev = currPosition;
+        }
+        return counter;
+    }
+}
