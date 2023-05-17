5b0e1ed352fe4b9aa4bba8dc0814118c36dd602d
vera <vero971410@gmail.com>
2023-05-17 19:09:51 +0200

    162. Find Peak Element

---
+++ src/medium/FindPeakElement_162.java
@@ -0,0 +1,17 @@
+package medium;
+
+public class FindPeakElement_162 {
+    // https://leetcode.com/problems/find-peak-element/description/
+    public int findPeakElement(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return end;
+    }
+}
