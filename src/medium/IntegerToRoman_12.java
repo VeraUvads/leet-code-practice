edb8c604905427cbfeda20b7bebf847797fc7786
vera <vero971410@gmail.com>
2023-07-31 16:03:04 +0200

    12. Integer to Roman

---
+++ src/medium/IntegerToRoman_12.java
@@ -0,0 +1,16 @@
+package medium;
+
+public class IntegerToRoman_12 {
+    int[] values = {1, 4, 5 , 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    String[] symbols = {"I", "IV","V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    public String intToRoman(int num) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = values.length - 1; i >= 0; i--) {
+            while (num >= values[i]) {
+                num -= values[i];
+                builder.append(symbols[i]);
+            }
+        }
+        return builder.toString();
+    }
+}
