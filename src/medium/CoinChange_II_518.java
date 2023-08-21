ee0aa13bf5b41d777c212f25795871127193727d
vera <vero971410@gmail.com>
2023-08-21 21:49:58 +0200

    518. Coin Change II

---
+++ src/medium/CoinChange_II_518.java
@@ -0,0 +1,16 @@
+package medium;
+
+public class CoinChange_II_518 {
+    // https://leetcode.com/problems/coin-change-ii/description/
+
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
